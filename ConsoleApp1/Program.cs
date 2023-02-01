4bf2299e44b32391e0e2851520143e0e46b91ef2
DilinDilin <mikasaasyna@gmail.com>
2023-02-01 11:56:27 +0300

    test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] readText = File.ReadAllLines(@"C:\Users\student\Desktop\new 1.txt");
+            foreach (string s in readText)
+            {
+                Console.WriteLine(s);
+                
+            }   
+        }
+    }
+}
